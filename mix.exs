83a257931bf4f7e33c8441aa8f84100502c972eb
johninvictus <invictusnitude@gmail.com>
2021-05-03 21:18:08 +0300

    added absinthe

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule QlBlog.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.6"}
     ]
   end
 
