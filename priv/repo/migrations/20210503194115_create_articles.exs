6eea023ad07984f88e94c2c6af996f65839c2d94
johninvictus <invictusnitude@gmail.com>
2021-05-03 22:52:47 +0300

    removed user for now and added blog schema

---
+++ priv/repo/migrations/20210503194115_create_articles.exs
@@ -0,0 +1,13 @@
+defmodule QlBlog.Repo.Migrations.CreateArticles do
+  use Ecto.Migration
+
+  def change do
+    create table(:articles) do
+      add :title, :string
+      add :content, :string
+
+      timestamps()
+    end
+
+  end
+end
